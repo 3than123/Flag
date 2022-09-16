b41e57a5e146d626cfa69b118b163ebd3614c73c
Ethan Langle <eman.soccer99@gmail.com>
2022-09-16 12:22:32 -0500

    Add java source

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+  public static void main(String[] a) {
+    System.out.println("");
+    Flag f1 = new Flag();
+    Flag f2 = new Flag(true);
+    Flag f3 = new Flag("Belgium","black,yellow, and red", true );
+  
+    System.out.println(f1);
+    System.out.println(f2);
+    System.out.println(f3);
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+
+    f3.fold();
+    f3.Ccolors();
+    f3.setWave(false);
+    f3.wind();
+   
+  }
+}
